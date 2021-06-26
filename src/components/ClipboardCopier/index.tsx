a7dec5a559e8e3c5902e2a0858f2e8ffb723d212
Bruno Souza <bruno.arubesu@gmail.com>
2021-06-26 19:57:14 -0300

    chore: add ClipboardCopier component

---
+++ src/components/ClipboardCopier/index.tsx
@@ -0,0 +1,24 @@
+import copyImg from '../assets/images/copy.svg';
+
+import { CopyToClipboardButton } from './styles';
+
+interface ClipboardCopierProps {
+  text: string;
+  label: string;
+}
+
+export const ClipboardCopier = ({ text, label }: ClipboardCopierProps) => {
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text)
+  }
+
+  return (
+    <CopyToClipboardButton onClick={copyToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy text" />
+      </div>
+      <span>{label} #{text}</span>
+    </CopyToClipboardButton>
+  )
+}
