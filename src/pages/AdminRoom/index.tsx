bd6f2180738873a739f0a57ce312c3460fe9b924
Bruno Souza <bruno.arubesu@gmail.com>
2021-07-11 14:46:37 -0300

    feat(admin room): add admin room page

---
+++ src/pages/AdminRoom/index.tsx
@@ -0,0 +1,36 @@
+import { useHistory, useParams } from 'react-router-dom'
+
+import { RoomHeader } from '../../components/Room/RoomHeader';
+import { RoomContainer } from '../../components/Room/Container';
+import { QuestionsBoardContainer } from '../../components/QuestionBoard/QuestionsBoardContainer';
+import { database } from '../../services/firebase';
+
+import '../styles/room.scss';
+import { QuestionList } from '../../components/QuestionBoard/QuestionList';
+
+type RoomParams = {
+  id: string;
+}
+
+export function AdminRoom() {
+  const history = useHistory()
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const handleCloseRoom = async () => {
+    await database.ref(`rooms/${roomId}`).update({
+      closedAt: new Date(),
+    })
+
+    history.push('/');
+  }
+
+  return (
+    <RoomContainer>
+      <RoomHeader roomId={roomId} handleCloseRoom={handleCloseRoom} />
+      <QuestionsBoardContainer roomId={roomId} >
+        <QuestionList roomId={roomId} isAdmin />
+      </QuestionsBoardContainer>
+    </RoomContainer>
+  );
+}
