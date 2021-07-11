49ce15599bef286e2f3e64eee8d13d75e3dc2ca9
Bruno Souza <bruno.arubesu@gmail.com>
2021-07-11 15:01:36 -0300

    chore: add Question component

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import { Container, Footer, UserInfo } from './styles';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+}
+
+export function Question({
+  content,
+  author,
+  children,
+}: QuestionProps) {
+  return (
+    <Container>
+      <p>{content}</p>
+      <Footer>
+        <UserInfo>
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </UserInfo>
+        <div>
+          {children}
+        </div>
+      </Footer>
+    </Container>
+  );
+}
